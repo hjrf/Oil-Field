b1afe7fe87101349da633b9f9ec4bfe4b76b3a84
hjr <hjr@DELL>
2016-09-12 18:14:36 +0800

    添加项目文件。

---
+++ mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc.Startup))]
+namespace mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
